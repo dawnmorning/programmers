5770432637d4557566d2e2e0eb85c0236d68a435
김종혁 <notify9637@naver.com>
2022-12-14 11:09:51 +0900

    큰 수

---
+++ 그리디/큰 수.py
@@ -0,0 +1,13 @@
+def solution(number, k):
+    # 정렬해서 큰 수를 찾는 것이 아니라, 차례대로 들어오는데, 그 안에서 최대 숫자 만드는 것
+    answer = ''
+    temp = []
+    # 숫자들 들어올 때 마다 먼저 있는 수보다 크다면, 앞의 수는 지우고 k(횟수) 차감
+    for i in number:
+        while temp and temp[-1] < i and k > 0:
+            k -= 1
+            temp.pop()
+        temp.append(i)
+    # 있는 숫자들 합치기
+    answer = ''.join(temp[:len(temp) - k])
+    return answer
